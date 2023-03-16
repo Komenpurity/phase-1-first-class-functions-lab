160fd53048fc1f5027a814cbe96ef24ae66f89a3
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-16 09:29:46 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = function (){
+    let result = console.log(drivers.slice(0,2));
+    return result;
+}
+returnFirstTwoDrivers()
+
+
+const returnLastTwoDrivers = function(){
+    let result = console.log(drivers.slice(2,4));
+    return result;
+}
+returnLastTwoDrivers()
+
+
+function selectingDrivers(){
+    const first = returnFirstTwoDrivers();
+    const last = returnLastTwoDrivers();
+}
+selectingDrivers();
+
+
+
+const fareMultiplier = function createFareMultiplier(a){
+    return a * a;
+}
+fareMultiplier(4);
+
+
+
